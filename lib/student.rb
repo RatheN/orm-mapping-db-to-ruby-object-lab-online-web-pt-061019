615908bede32632493dd4fce22209ed9cc3192eb
Nathan Rathe <nprathe10@gmail.com>
2019-08-26 23:04:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,6 +66,14 @@ class Student
       SELECT * FROM students WHERE grade = 9
     SQL
 
-    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql).map {|r| self.new_from_db(r)}
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map {|r| self.new_from_db(r)}
   end
 end
