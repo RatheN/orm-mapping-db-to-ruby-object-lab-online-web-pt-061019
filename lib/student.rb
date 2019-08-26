8df0b38e0b2da7c8ef4c0da7d2452f01012e8957
Nathan Rathe <nprathe10@gmail.com>
2019-08-26 23:02:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
 
   def self.all_students_in_grade_9
     sql = <<-SQL
-      SELECT * FROM students WHERE grade = "9th"
+      SELECT * FROM students WHERE grade = 9
     SQL
 
     DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
